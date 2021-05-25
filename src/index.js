7b75b766ebc9861e523a51d25e27f13d7bbaec21
Jake Shedroff <shedroji@mail.uc.edu>
2021-05-24 20:53:56 -0700

    remove old store

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import store from './store/phone-book/store'
 import { Provider } from 'react-redux';
 
-import { initialStore } from './store';
 import Main from './Main';
 
 const App = () => {
